6df282f880f13f8fa35c47e0e08e5a84d84aa74c
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2021-04-12 15:41:46 +0100

    Lexer

--- src/bake.cpp
+++ src/bake.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
 
-int main(int argc, const char* argv[]) {
-    std::cout << "Hello World!\n";
-    return 0;
+enum Token {
+  tok_eof = -1,
+  tok_base = -2,
+  tok_topping = -3,
+  tok_identifier = -4,
+  tok_number = -5
+};
+
+
+static std::string IdentifierStr;
+static double NumVal;
+
+static int gettok() {
+  static int LastChar = ' ';
+
+  while (isspace(LastChar))
+    LastChar = getchar();
+
+  if (isalpha(LastChar)) {
+    IdentifierStr = LastChar;
+    while (isalnum((LastChar = getchar())))
+      IdentifierStr += LastChar;
+
+    if (IdentifierStr == "base")
+      return tok_base;
+    if (IdentifierStr == "topping")
+      return tok_topping;
+    return tok_identifier;
+  }
+
+  if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = strtod(NumStr.c_str(), nullptr);
+    return tok_number;
+  }
+
+  if (LastChar == '#') {
+    do
+      LastChar = getchar();
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF)
+      return gettok();
+  }
+
+  // Check for end of file.  Don't eat the EOF.
+  if (LastChar == EOF)
+    return tok_eof;
+
+  // Otherwise, just return the character as its ascii value.
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
+
+int main() {
+  while(true) {
+    int tok = gettok();
+    std::cout << "got token: " << tok << std::endl;
+  }
+  return 0;
 }
